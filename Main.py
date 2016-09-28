c3154b587cefb1d6497f036fcced56f939a8b6b8
Corné Wever <129091@hondsrugcollege.nl>
2016-09-28 15:35:48 +0200

    Kofi
    Kofi

--- Main.py
+++ Main.py
@@ -3,7 +3,8 @@ from User import User
 Jelte = User("Jelte", "1234", "129329@hondsrugcollege.nl")
 Matthijs = User("Matthijs", "4321", "129472@hondsrugcollege.nl")
 Jesper = User("Jesper", "0000", "129175@hondsrugcollege.nl")
-
+Corné = User ("Corné", "6969", "129091@hondsrugcollege.nl")
 print(Jelte)
 print(Matthijs)
 print(Jesper)
+print(Corné)
